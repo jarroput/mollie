10a3f04a4614e1256e082878cec18133c1fb9f65
Jarro Put <put.jarro@gmail.com>
2018-11-18 10:02:05 +0100

    Make app ready for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,29 +1,43 @@
 defmodule Molliex.MixProject do
   use Mix.Project
 
+  @description """
+  Elixir wrapper for the Mollie API
+  """
+
   def project do
     [
       app: :molliex,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: @description,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:httpoison, "~> 0.8"},
       {:jason, "~> 1.1"},
-      {:credo, "~> 1.0", only: [:test, :dev], runtime: false}
+      {:credo, "~> 1.0", only: [:test, :dev], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: "mollie_ex",
+      maintainers: ["Jarro Put"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/jarroput/molliex"}
     ]
   end
 end
