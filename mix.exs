aa036da6f89671c2b31bd1f86a0db72a8906ff40
Jarro Put <put.jarro@gmail.com>
2019-08-28 13:56:32 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Mollie.MixProject do
   def project do
     [
       app: :mollie,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.5",
       description: @description,
       start_permanent: Mix.env() == :prod,
