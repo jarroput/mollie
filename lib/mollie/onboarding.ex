de4a4330980b18e4d7f532374e91ec1c93fe11d0
Jarro Put <put.jarro@gmail.com>
2019-10-30 11:22:05 +0100

    Add Onboarding API

---
+++ lib/mollie/onboarding.ex
@@ -0,0 +1,56 @@
+defmodule Mollie.Onboarding do
+  import Mollie
+  alias Mollie.Client
+
+  @doc """
+  Get the status of onboarding of the authenticated organization
+
+  ## Example
+
+      Mollie.Onboarding.me client
+
+  More info at: https://docs.mollie.com/reference/v2/onboarding-api/get-onboarding-status
+  """
+  @spec me(Client.t()) :: Mollie.response()
+  def me(client) do
+    get("v2/onboarding/me", client)
+  end
+
+  @doc """
+  Submit data that will be prefilled in the merchant's onboarding.
+  Please note that the data you submit will only be processed when the onboarding status is `needs-data`.
+
+  Onboarding body example
+  ```
+  %{
+    "organization" => %{
+      "name" => "Mollie B.V.",
+      "address" => %{
+          "streetAndNumber" => "Keizersgracht 313",
+          "postalCode" => "1018 EE",
+          "city" => "Amsterdam",
+          "country" => "NL"
+      },
+      "registrationNumber" => "30204462",
+      "vatNumber" => "NL815839091B01"
+    },
+    "profile" => %{
+      "name" => "Mollie",
+      "url" => "https =>//www.mollie.com",
+      "email" => "info@mollie.com",
+      "phone" => "+31208202070",
+      "categoryCode" => 6012
+    }
+  }
+  ```
+
+  ## Example
+
+      Mollie.Onboarding.submit client, onboarding_body
+
+  More info at: https://docs.mollie.com/reference/v2/onboarding-api/submit-onboarding-data
+  """
+  def submit(client, params \\ %{}) do
+    post("v2/onboarding/me", client, params)
+  end
+end
