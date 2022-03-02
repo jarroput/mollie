6691f412c1a9e8b342799c10f6061808a9c17637
Jarro Put <jarroput@users.noreply.github.com>
2022-03-02 14:28:26 +0100

    Fix docs indentation

--- lib/mollie/profiles/methods.ex
+++ lib/mollie/profiles/methods.ex
@@ -60,7 +60,8 @@ defmodule Mollie.Profiles.Methods do
 
 
   ## Example
-  Mollie.Profiles.Methods.disable client, "ideal"
+  
+      Mollie.Profiles.Methods.disable client, "ideal"
 
   More info at: https://docs.mollie.com/reference/v2/profiles-api/enable-method
   """
